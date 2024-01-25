27652fd9dc53bfe7749617af42d9ee2949c53e4b
nana <goldbarrr5@gmail.com>
2024-01-25 21:37:47 +0900

    비즈니스 스와이퍼 배경

--- js/main.js
+++ js/main.js
@@ -10,20 +10,37 @@ $(function () {
     menu: '#indicator',
   });
 
+  // Swiper 초기화
   const businessSlider = new Swiper('.business-slide', {
-    // 옵션 설정
-    loop: true,
     slidesPerView: 7,
+    loop: true,
     spaceBetween: 20,
-    // 페이지네이션
+    loopAdditionalSlides: 3,
     pagination: {
       el: '.swiper-pagination',
       type: 'fraction',
     },
-    // Navigation arrows
     navigation: {
       nextEl: '.btn-next',
       prevEl: '.btn-prev',
     },
+    on: {
+      slideChange: function () {
+        const idx = this.realIndex;
+        const businessBg = $(this.slides[idx]).data('bg');
+        $('.business').css({
+          'background-image': `url(${businessBg})`,
+          transition: 'background-image 0.5s ease-in-out',
+        });
+      },
+    },
+  });
+  $('.swiper-slide').on('click', function () {
+    // 클릭한 슬라이드에 active 클래스 추가
+    $(this).addClass('active').siblings().removeClass('active');
+
+    // 클릭한 슬라이드의 인덱스를 가져와서 활성화된 슬라이드로 이동
+    const clickedIndex = $(this).index();
+    businessSlider.slideToLoop(clickedIndex);
   });
 });
